1fe2c66f3072cf6fb3a7e40f12ce71331018e04f
Jeff Kent <jeff@jkent.net>
2020-05-28 18:13:25 -0500

    drop python2.7 in manylinux

--- .ci/build_linux_wheels.sh
+++ .ci/build_linux_wheels.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-for PYBIN in /opt/python/*/bin; do
+for PYVER in "cp35-cp35m" "cp36-cp36m" "cp37-cp37m" "cp38-cp38"; do
+  PYBIN="/opt/python/${PYVER}/bin"
   "${PYBIN}/python" -m pip install wheel
   "${PYBIN}/python" setup.py bdist_wheel
 done
