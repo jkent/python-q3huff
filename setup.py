88566196902bcb2773f363a110ef9ccd124061d0
Jora Kornev <JKornev@users.noreply.github.com>
2022-06-30 21:09:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import Extension, setup
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 huffman_src = ['src/hufflib.c',
                'src/huffman.c',
@@ -12,17 +12,17 @@ huffman_src = ['src/hufflib.c',
 huffman_dep = ['src/qcommon.h',
                'src/q_shared.h']
 
-huffman_ext = Extension('q3huff', huffman_src, depends=huffman_dep,
+huffman_ext = Extension('q3huff2', huffman_src, depends=huffman_dep,
                         extra_compile_args=['-std=c99'])
 
-setup(name='q3huff', version = __version__,
+setup(name='q3huff2', version = __version__,
       ext_modules = [huffman_ext],
       description = 'Python 3 C API wrapper for Huffman code found in ioquake3.',
       author = 'Jeff Kent',
       author_email = 'jeff@jkent.net',
       maintainer = 'Jeff Kent',
       maintainer_email = 'jeff@jkent.net',
-      url = "https://github.com/jkent/python-q3huff",
+      url = "https://github.com/JKornev/python-q3huff",
       license = 'GNU GPLv2',
       classifiers = [
         'Development Status :: 4 - Beta',
